57701f94e223a12f50c4597db47da8ae2ebadf50
ywur7t <118150868+ywur7t@users.noreply.github.com>
2022-12-11 22:54:26 +1000

    Add files via upload

---
+++ ClassWorld/ClassWorld/ClassWorld.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include<string>
+#include "HeaderOfClass.h"
+
+int main()
+{
+    std::cout << "+--------------------+\n";
+    program::elf person;
+    person.name = "Stive";             std::cout << "|name       | " << person.name <<"\n";
+    person.world = "c1";                 std::cout << "|world      | " << person.world <<"\n";
+    person.age = 20;                        std::cout << "|age        | " << person.age <<"\n";
+    person.mind = true;                  std::cout << "|mind       | " << person.mind <<"\n";
+    person.magic = true;                 std::cout << "|magic      | " << person.magic <<"\n";
+    person.profession = "hunter"; std::cout << "|profession | " << person.profession <<"\n";
+    person.light = true;                   std::cout << "|light      | " << person.light << "\n";
+    person.power = 1247;              std::cout << "|power      | " << person.power <<"\n";
+    person.growth = 177;               std::cout << "|growth     | " << person.growth <<"\n";
+    person.weight = 57;                  std::cout << "|weight     | " << person.weight <<"\n";
+    
+    std::cout << "+-------animal-------+\n";
+    program::wolf wolf;
+    wolf.name = "Wolf";                      std::cout << "|name       | " << wolf.name << "\n";
+    wolf.world = "c1";                          std::cout << "|world      | " << wolf.world << "\n";
+    wolf.zone = "forest";                     std::cout << "|zone       | " << wolf.zone << "\n";
+    wolf.power = 1444;                       std::cout << "|power      | " << wolf.power << "\n";
+     std::cout << "|danger     | " << wolf.dangerCheck(person, wolf) << "\n";
+    wolf.one = true;                             std::cout << "|one        | " << wolf.one << "\n";
+    wolf.growth = 244;                        std::cout << "|growth     | " << wolf.growth << "\n";
+    wolf.weight = 289;                        std::cout << "|weight     | " << wolf.weight << "\n";
+    
+    
+    std::cout << "+--------------------+\n";
+    program::ghost phantom;
+    phantom.name = "Phantom";                                         std::cout << "|name       | " << phantom.name << "\n";
+    phantom.age = 629;                                                          std::cout << "|age        | " << phantom.age << "\n";
+    phantom.power = 5000;                                                   std::cout << "|power      | " << phantom.power << "\n";
+                                                                                                  std::cout << "|danger     | " << phantom.dangerCheck(person, phantom) << "\n";
+    phantom.fear = "incense";                                               std::cout << "|fear       | " << phantom.fear << "\n";
+    phantom.real = false;                                                         std::cout << "|real       | " << phantom.real << "\n";
+        
+    
+    std::cout << "+--------group--------+\n";
+    program::ghost phantom1;    phantom1.power = 4000; phantom1.name = "Phantom"; phantom1.age = 300;
+
+    program::creature group;
+    group.name = "phantom + phantom1";                                  std::cout << "|name       | " << group.name << "\n";
+    group = phantom + phantom1;      std::cout << "|power      | " << group.power << "\n";
+    std::cout << "|danger     | " << group.dangerCheck(person, phantom) << "\n";
+
+    phantom == phantom1 ? std::cout << "phantom = phantom1" : std::cout << "phantom != phantom1";
+
+    phantom>phantom1? std::cout << "\nphantom older&powerful then phantom1\n" : std::cout << "\nphantom !(older&powerful then) phantom1\n";
+    
+    phantom < phantom1 ? std::cout << "\nphantom !(older&powerful then) phantom1\n" : std::cout << "\nphantom older&powerful then phantom1\n";
+    return 0;
+}
---
+++ ClassWorld/ClassWorld/HeaderOfClass.h
@@ -0,0 +1,134 @@
+#pragma once
+
+namespace program
+{
+	class basic_parameters
+	{
+	public:
+		std::string name;
+		double age;
+		std::string world;
+		double weight;
+		double growth;
+		double power;
+	};
+	///////////////////////////
+	//////////////////////////
+	class anthropomorthic :public basic_parameters
+	{
+	public:		
+		bool mind;
+		bool magic;
+		std::string profession;
+	};
+	
+	
+	class triton :public anthropomorthic
+	{
+	public:
+		double tail_length;
+	};
+	class elf :public anthropomorthic
+	{
+	public:
+		bool light;
+	};
+	
+	///////////////////////////
+	//////////////////////////
+	class animals :public basic_parameters
+	{
+	public:
+		
+		std::string zone;
+		
+		double dangerCheck(anthropomorthic obj1, animals obj2)
+		{
+			if (obj1.power > obj2.power)
+				return danger = 0;
+			else
+			{
+				if (obj1.power > obj2.power / 2)
+					return danger = 1;
+				else return danger = 2;
+			}
+		}
+
+	private:
+		int danger;
+	};
+
+	class wolf :public animals
+	{
+	public:
+		bool one;
+	};
+	class bird :public animals
+	{
+	public:
+		friend bool size(bird );
+		bool fly;
+	private:
+		bool size;
+	};
+
+	bool size(bird obj)
+	{
+		obj.fly == true && obj.weight < 5 ?  obj.size = true :  obj.size = false;
+		return obj.size;
+	}
+
+	///////////////////////////
+	//////////////////////////
+	class creature :public animals
+	{
+	public:
+		bool real;
+
+		creature operator+(creature& obj1)
+		{
+			creature obj2;
+			obj2.power =  power+obj1.power;
+			return obj2;
+		}
+		creature operator=(const creature& obj1)
+		{
+			this->power = obj1.power;
+			return *this;
+		}
+		bool operator==(const creature& obj1)
+		{
+			return (power == obj1.power) && (name == obj1.name);
+		 }
+		bool operator>(const creature& obj1)
+		{
+			return (age > obj1.age) && (power>obj1.power);
+		}
+		bool operator<(const creature& obj1)
+		{
+			return !((age > obj1.age) && (power > obj1.power));
+		}
+
+	};
+
+
+	
+
+
+
+
+	class ghost :public creature
+	{
+	public:
+		std::string fear;
+	};
+	class sirenas :public creature
+	{
+	public:
+		double speed;
+	};
+
+	
+
+
+}
