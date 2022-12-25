7970b82554b4770a278bc2f9b7efe7e902bce51d
ywur7t <118150868+ywur7t@users.noreply.github.com>
2022-12-25 11:09:25 +1000

    Update ClassWorld.cpp

--- ClassWorld/ClassWorld/ClassWorld.cpp
+++ ClassWorld/ClassWorld/ClassWorld.cpp
@@ -6,7 +6,7 @@ int main()
 {
     std::cout << "+--------------------+\n";
     program::elf person;
-    person.name = "Stive";             std::cout << "|name       | " << person.name <<"\n";
+    person.name = "Stive";             person.NamePrint(person.name);
     person.world = "c1";                 std::cout << "|world      | " << person.world <<"\n";
     person.age = 20;                        std::cout << "|age        | " << person.age <<"\n";
     person.mind = true;                  std::cout << "|mind       | " << person.mind <<"\n";
@@ -19,7 +19,7 @@ int main()
     
     std::cout << "+-------animal-------+\n";
     program::wolf wolf;
-    wolf.name = "Wolf";                      std::cout << "|name       | " << wolf.name << "\n";
+    wolf.name = "Wolf";                      wolf.NamePrint(wolf.name);
     wolf.world = "c1";                          std::cout << "|world      | " << wolf.world << "\n";
     wolf.zone = "forest";                     std::cout << "|zone       | " << wolf.zone << "\n";
     wolf.power = 1444;                       std::cout << "|power      | " << wolf.power << "\n";
@@ -41,24 +41,27 @@ int main()
     
     std::cout << "+--------group--------+\n";
     program::ghost phantom1;    phantom1.power = 4000; phantom1.name = "Phantom"; phantom1.age = 300;
-
     program::creature group;
     group.name = "phantom + phantom1";                                  std::cout << "|name       | " << group.name << "\n";
     group = phantom + phantom1;      std::cout << "|power      | " << group.power << "\n";
     std::cout << "|danger     | " << group.dangerCheck(person, phantom) << "\n";
-
-    phantom == phantom1 ? std::cout << "phantom = phantom1" : std::cout << "phantom != phantom1";
-
-    phantom>phantom1? std::cout << "\nphantom older&powerful then phantom1\n" : std::cout << "\nphantom !(older&powerful then) phantom1\n";
-    
-    phantom < phantom1 ? std::cout << "\nphantom !(older&powerful then) phantom1\n" : std::cout << "\nphantom older&powerful then phantom1\n";
+    phantom == phantom1 ? std::cout << "|phantom = phantom1" : std::cout << "|phantom != phantom1";
+    //phantom>phantom1? std::cout << "\n| phantom older&powerful then phantom1\n" : std::cout << "\n| phantom !(older&powerful then) phantom1\n";
+    phantom < phantom1 ? std::cout << "\n|phantom !(older&powerful then) phantom1\n\n" : std::cout << "\n|phantom older&powerful then phantom1\n\n";
 
     program::bird bird;
-    
+    bird.name = "kenny";
     bird.fly = true;
     bird.weight = 2;
     program::size(bird);
     program::printfunc(bird);
+
+    program::mimc mimic(std::move(person));
+    std::cout<< "\n+--------mimic---------+\n";
+    std::cout << "|mimic age is "<<mimic.age << "      | person age is " << person.age << "\n";
+    std::cout << "|mimic power is " << mimic.power << "  | person power is " << person.power << "\n";
+    std::cout << "|mimic weight is " << mimic.weight << "   | person weight is " << person.weight << "\n";
+    std::cout << "|mimic growth is " << mimic.growth << "  | person growth is " << person.growth << "\n";
 
     return 0;
 }
